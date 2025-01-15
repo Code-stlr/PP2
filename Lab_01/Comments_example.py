f23f0be7ab3aab4b0e5cd4e17f3526b5c563c106
Ehsan <mrehsankz@gmail.com>
2025-01-15 01:35:37 -0700

    Adding Comments File

---
+++ Lab_01/Comments_example.py
@@ -0,0 +1,22 @@
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World! Again !") #This is a comment
+
+
+#print("Hello, World!")
+print("Its chewsday init?")
+
+#This is a comment
+#written in
+#more than just one line
+print("Multy line comment ?")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Again ?")
+
+
