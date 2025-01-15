3f15655d2fa5e17fbc1f4e454dc8c3cea98e2ddd
Ehsan <mrehsankz@gmail.com>
2025-01-15 01:47:28 -0700

    Assign Multiple Values Examples

---
+++ Lab_01/AMV_example.py
@@ -0,0 +1,19 @@
+# Python Variables - Assign Multiple Values
+
+x, y, z = "Orange", "Banana", "Watermelon"
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+x = y = z = "Bananaaa"
+print(x)
+print(y)
+print(z)
+
+# Unpack a Collection
+fruits = ["Pineapple", "banana", "Watermelon"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
