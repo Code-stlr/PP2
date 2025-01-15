83844c059a57344a280e3d7d6c9d9a206bbff981
Ehsan <mrehsankz@gmail.com>
2025-01-15 01:47:59 -0700

    Variable Examples

---
+++ Lab_01/VariableNames_example.py
@@ -0,0 +1,8 @@
+#Variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+
+myvar = "Ehsan"
+my_var = "Ehsan"
+_my_var = "Ehsan"
+myVar = "Ehsan"
+MYVAR = "Ehsan"
+myvar2 = "Ehsan"
---
+++ Lab_01/Variables_example.py
@@ -0,0 +1,27 @@
+# example 1
+x = 5
+y = "Ehsan"
+print(x)
+print(y)
+
+# example 2 
+x = 4       # x is of type int
+x = "Ehsan" # x is now of type str
+print(x)
+
+# example 3 Casting 
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# example 4 Type 
+x = 5
+y = "Ehsan"
+print(type(x))
+print(type(y))
+
+#Example 5 Case Sensitive 
+a = 4
+A = "Ehsan"
+    #A will not overwrite a
+
