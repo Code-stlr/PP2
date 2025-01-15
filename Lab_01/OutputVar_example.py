61b448933846d39d8a3184f1abb8822078f7d11b
Ehsan <mrehsankz@gmail.com>
2025-01-15 01:47:46 -0700

    Output Variables Example

---
+++ Lab_01/OutputVar_example.py
@@ -0,0 +1,24 @@
+# Python - Output Variables
+
+x = "Python is Slow"
+print(x)
+
+a = "Python"
+b = "is"
+c = "Slow"
+print(a, b, c)
+
+
+x = "Python "
+y = "is "
+z = "awesome(ok)"
+print(x + y + z)
+
+#numbers 
+x = 5
+y = 10
+print(x + y)
+# Both
+x = 5
+y = "Ehsan"
+print(x, y)
