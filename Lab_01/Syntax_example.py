150884f2c6233a850640ab29fbc32bbf3a55c2aa
Ehsan <mrehsankz@gmail.com>
2025-01-15 01:24:03 -0700

    rename file and folder

---
+++ Lab_01/Syntax_example.py
@@ -0,0 +1,18 @@
+
+## example one 
+if 5 > 2:
+  print("Five is greater than two!")
+
+## example 2 
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+## example 3
+x = 5
+y = "Hello, World!"
+
+## example 4
+#This is a comment.
+print("Hello, World!")
